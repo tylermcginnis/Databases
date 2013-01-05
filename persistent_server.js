cd10e662b422acc699a7efc6763e551b75e70a6f
Jono Xia <jono@fastmail.fm>
2013-01-05 09:07:42 -0800

    Created mostly complete tests - students will need to customize some of it to match the schema they create.

--- persistent_server.js
+++ persistent_server.js
@@ -8,14 +8,16 @@ var dbConnection = mysql.createClient({
   database: "chat"
 });
 /* You'll need to fill this out with your mysql username and password.
- * This is specifying that we're using the database called "chat", which we
- * created by running schema.sql.*/
+ * database: "chat" specifies that we're using the database called 
+ * "chat", which we created by running schema.sql.*/
 
 dbConnection.connect();
-
 /* Now you can make queries to the Mysql database using the
  * dbConnection.query() method.
  * See https://github.com/felixge/node-mysql for more details about
  * using this module.*/
+
+/* You already know how to create an http server from the previous
+ * assignment; you can re-use most of that code here. */
 
 dbConnection.end();
