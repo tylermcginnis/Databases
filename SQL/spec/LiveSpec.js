aee1645ae07692da9f662a49a5ebf7c289010ee5
Hack Reactor Students <github@hackreactor.com>
2013-08-30 20:21:38 -0700

    all tests passing, first part done

--- SQL/spec/LiveSpec.js
+++ SQL/spec/LiveSpec.js
@@ -34,17 +34,23 @@ describe("Persistent Node Chat Server", function() {
              uri: "http://127.0.0.1:8080/messages/general",
              json: {
                 username: "Valjean",
-                message: "In mercy's name, three days is all I need." }
+                message: 'first message',
+                roomname: 'general'
+              }
             },
             function(error, response, body) {
+
+              console.log("RESPONSE", response);
               dbConnection.query("SELECT * FROM messages",
+
                 function(err, results) {
                   if(err){
                     console.log("THIS IS THE ERROR: ", err);
                   }
+                  console.log("RESULT", results);
                   expect(results.length).toEqual(1);
                   expect(results[0].username).toEqual("Valjean");
-                  expect(results[0].message).toEqual('the text');
+                  expect(results[0].message).toEqual('first message');
                   /* TODO: You will need to change these tests if the
                    * column names in your schema are different from
                    * mine! */
@@ -57,7 +63,7 @@ describe("Persistent Node Chat Server", function() {
   it("Should output all messages from the DB", function(done) {
     // Let's insert a message into the db
     var queryString = "INSERT INTO messages SET ?";
-    var queryArgs = {username: "Javert", message: "Men like you can never change!"}//["Javert", "Men like you can never change!"];
+    var queryArgs = {username: "Javert", message: "Men like you can never change!", roomname: 'general'}//["Javert", "Men like you can never change!"];
     /* TODO - The exact query string and query args to use
      * here depend on the schema you design, so I'll leave
      * them up to you. */
@@ -66,8 +72,12 @@ describe("Persistent Node Chat Server", function() {
       function(err, results, fields) {
         /* Now query the Node chat server and see if it returns
          * the message we just inserted: */
-        request("http://127.0.0.1:8080/messages/",
+         for(var i = 0; i <10000; i++){
+           console.log(fields);
+         }
+        request("http://127.0.0.1:8080/messages/general",
           function(error, response, body) {
+            console.log("THIS IS THE BODY",body);
             var messageLog = JSON.parse(body);
             expect(messageLog[0].username).toEqual("Javert");
             expect(messageLog[0].message).toEqual("Men like you can never change!");
